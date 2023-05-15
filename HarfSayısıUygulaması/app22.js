c1b8dea4067ff7de655de8726657709159d5690f
RAZİYE DOGAN <98969893+raziyedogan@users.noreply.github.com>
2023-05-15 09:28:19 +0300

    Add files via upload

---
+++ HarfSayısıUygulaması/app22.js
@@ -0,0 +1,16 @@
+// kullanıcı tarafından girilen cümle içerisinde kullanıcı tarafından girilen harfin kaç adet bulunduğunu döndüren kod.
+
+let metin = prompt("Cümle giriniz.");
+let harf = prompt("Cümle içerisinde adetini hesaplamak istediğiniz harfi giriniz.");
+let cnt=0;
+
+alert(harf + " harfi cümle içerisinde " + harf_sayaci(metin,harf) + " adet bulunmaktadır.");
+
+function harf_sayaci(metin,harf){
+    for(let i=0; i<metin.length; i++){
+        if(metin.charAt(i) === harf){
+            cnt++;
+        }
+    }
+    return cnt;
+}
